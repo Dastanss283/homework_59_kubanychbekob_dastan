4df07c9170f96c39b77d7d2d2e2b5358e820d687
dosya_nurbay <dosya@mail.ru>
2020-04-08 09:31:54 +0900

    Добавлена 3-Задание

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ var doubleClick = document.getElementsByClassName("d-bloc kw-100")[0];
 	 	var like = document.getElementsByClassName("fas fa-heart")[0];
 		like.style.color = (like.style.color == 'gray') ? 'red' : 'gray';
 })
+
+var post = document.getElementsByClassName("fas fa-bookmark")[0];
+post.addEventListener("click", function(){
+	this.style.color = (this.style.color == 'black') ? '#DCFFEF' : 'black';
+})
+
