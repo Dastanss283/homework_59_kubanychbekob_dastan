70c1ea450044f421d7ffd3fa2802b163318c559a
dosya_nurbay <dosya@mail.ru>
2020-04-09 18:32:53 +0900

    last commit

--- main.js
+++ main.js
@@ -14,3 +14,9 @@ post.addEventListener("click", function(){
 	this.style.color = (this.style.color == 'black') ? '#DCFFEF' : 'black';
 })
 
+function  hideSplashScreen(){
+  let win = document.getElementById("page-splash");
+  let splash = document.getElementById('noscroll');
+  win.style.visibility = "hidden";
+    splash.classList.remove("no-scroll");
+}
